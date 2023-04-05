c7d4c9668d2d533b12ae87f1a0650ef4218ac0c4
themikail <hello@mikailgokce.com>
2023-04-05 10:59:38 +0200

    finish exercise

--- js/index.js
+++ js/index.js
@@ -5,9 +5,13 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("div");
+  newToast.classList.add("toast");
+  newToast.textContent = "A new toast message";
+
+  toastContainer.appendChild(newToast);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
